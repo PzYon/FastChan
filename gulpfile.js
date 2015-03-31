f7f4dfa6bff239bc19316a87710636e7122effb6
Markus <markus.doggweiler@gmail.com>
2015-03-31 07:20:00 +0200

    bundled all libraries into libs/packaged.js using gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,32 @@
+// todo:
+// - bundle app-scripts
+// - minify
+// - create different configuration for prod and dev
+// - register in WS: https://www.jetbrains.com/webstorm/help/run-debug-configuration-gulp-js.html
+
 var gulp = require("gulp");
+var concat = require('gulp-concat');
 
 var bowerDir = "./bower_components/";
-var clientScriptDir = "./client/scripts/packages/";
+var clientScriptDir = "./client/scripts/libs/";
 
-gulp.task("default", function() {
-	gulp.start("copyPackages");
+gulp.task("default", function () {
+    gulp.start("copyPackages");
 });
 
-gulp.task("copyPackages", function(){
-	gulp.src(bowerDir + "angular/angular.js").pipe(gulp.dest(clientScriptDir + "angular"));
-	gulp.src(bowerDir + "angular-route/angular-route.js").pipe(gulp.dest(clientScriptDir + "angular-route"));
-	gulp.src(bowerDir + "angular-ui-ace/ui-ace.js").pipe(gulp.dest(clientScriptDir + "angular-ui-ace"));
-	gulp.src(bowerDir + "underscore/underscore.js").pipe(gulp.dest(clientScriptDir + "underscore"));
-	gulp.src(bowerDir + "socket.io-client/socket.io.js").pipe(gulp.dest(clientScriptDir + "socket.io-client"));
-	gulp.src(bowerDir + "socketio-file-upload/client.js").pipe(gulp.dest(clientScriptDir + "socketio-file-upload"));
-	gulp.src(bowerDir + "ace-builds/src/ace.js").pipe(gulp.dest(clientScriptDir + "ace"));
-	gulp.src(bowerDir + "ace-builds/src/*json.js").pipe(gulp.dest(clientScriptDir + "ace"));
-	gulp.src(bowerDir + "ace-builds/src/*javascript.js").pipe(gulp.dest(clientScriptDir + "ace"));
-	gulp.src(bowerDir + "ace-builds/src/*csharp.js").pipe(gulp.dest(clientScriptDir + "ace"));
-	gulp.src(bowerDir + "ace-builds/src/*html.js").pipe(gulp.dest(clientScriptDir + "ace"));
-	gulp.src(bowerDir + "ace-builds/src/*css.js").pipe(gulp.dest(clientScriptDir + "ace"));
+gulp.task("copyPackages", function () {
+    gulp.src([
+        bowerDir + "angular/angular.js",
+        bowerDir + "angular-route/angular-route.js",
+        bowerDir + "angular-ui-ace/ui-ace.js",
+        bowerDir + "underscore/underscore.js",
+        bowerDir + "socket.io-client/socket.io.js",
+        bowerDir + "socketio-file-upload/client.js",
+        bowerDir + "ace-builds/src/ace.js",
+        bowerDir + "ace-builds/src/*json.js",
+        bowerDir + "ace-builds/src/*javascript.js",
+        bowerDir + "ace-builds/src/*csharp.js",
+        bowerDir + "ace-builds/src/*html.js",
+        bowerDir + "ace-builds/src/*css.js"
+    ]).pipe(concat("packaged.js")).pipe(gulp.dest(clientScriptDir));
 });
