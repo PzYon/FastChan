db490933ef026131d718ec6f5de56aaed627d802
Markus <markus.doggweiler@gmail.com>
2015-03-31 07:47:49 +0200

    improved/fixed behavior in gulp - fixed issues introduced with packaging: ace is treated separately (not packaged) to ensure worker and mode files aren't always loaded and in order to have correct behavior, when ace dynamically loads scripts - added gulp-clean and use to clean libs-folder in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,42 @@
-// todo:
+// todos / nice to haves:
 // - bundle app-scripts
 // - minify
 // - create different configuration for prod and dev
 // - register in WS: https://www.jetbrains.com/webstorm/help/run-debug-configuration-gulp-js.html
 
 var gulp = require("gulp");
+var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 
 var bowerDir = "./bower_components/";
-var clientScriptDir = "./client/scripts/libs/";
+var clientLibsDir = "./client/scripts/libs/";
 
 gulp.task("default", function () {
-    gulp.start("copyPackages");
+    gulp.start("copyLibs");
 });
 
-gulp.task("copyPackages", function () {
+gulp.task("cleanLibs", function () {
+    return gulp.src(clientLibsDir, {read: false}).pipe(clean());
+});
+
+gulp.task("copyLibs", ["cleanLibs"], function () {
     gulp.src([
         bowerDir + "angular/angular.js",
         bowerDir + "angular-route/angular-route.js",
-        bowerDir + "angular-ui-ace/ui-ace.js",
         bowerDir + "underscore/underscore.js",
         bowerDir + "socket.io-client/socket.io.js",
-        bowerDir + "socketio-file-upload/client.js",
-        bowerDir + "ace-builds/src/ace.js",
-        bowerDir + "ace-builds/src/*json.js",
-        bowerDir + "ace-builds/src/*javascript.js",
-        bowerDir + "ace-builds/src/*csharp.js",
-        bowerDir + "ace-builds/src/*html.js",
-        bowerDir + "ace-builds/src/*css.js"
-    ]).pipe(concat("packaged.js")).pipe(gulp.dest(clientScriptDir));
+        bowerDir + "socketio-file-upload/client.js"
+    ]).pipe(concat("packaged.js")).pipe(gulp.dest(clientLibsDir));
+
+    // we do not package ace and related js-files, as otherwise we get issues
+    // regarding the basePath etc., when ace tries to dynamically load
+    // modes and workers from the (wrong) implicit path
+    gulp.src(bowerDir + "angular-ui-ace/ui-ace.js").pipe(gulp.dest(clientLibsDir + "ui-ace"));
+    gulp.src(bowerDir + "ace-builds/src/ace.js").pipe(gulp.dest(clientLibsDir + "ace"));
+
+    gulp.src(bowerDir + "ace-builds/src/*json.js").pipe(gulp.dest(clientLibsDir + "ace"));
+    gulp.src(bowerDir + "ace-builds/src/*javascript.js").pipe(gulp.dest(clientLibsDir + "ace"));
+    gulp.src(bowerDir + "ace-builds/src/*csharp.js").pipe(gulp.dest(clientLibsDir + "ace"));
+    gulp.src(bowerDir + "ace-builds/src/*html.js").pipe(gulp.dest(clientLibsDir + "ace"));
+    gulp.src(bowerDir + "ace-builds/src/*css.js").pipe(gulp.dest(clientLibsDir + "ace"));
 });
